69925aa6692028b5250edcbe30ab6026db6727c8
Danimar Ribeiro <danimaribeiro@gmail.com>
2014-08-17 00:05:07 -0300

    Feito for até 10000 para testar velocidade

--- RqTest.py
+++ RqTest.py
@@ -11,11 +11,17 @@ import requests
 if __name__ == '__main__':
     diretorio = './xmls_teste/'
     onlyfiles = [ join(diretorio,f) for f in listdir(diretorio) if isfile(join(diretorio,f)) ]
+    total = 0
     for i in range(0,100):
         for item in onlyfiles:
             with open(item, 'rb') as f:
                 conteudo = f.read()            
                 r = requests.post('http://localhost:5000/envio_xml', data=conteudo)
                 print r.status_code
+                total += 1
+                if total>=10000:
+                    break
+        if total>=10000:
+            break
         
     
